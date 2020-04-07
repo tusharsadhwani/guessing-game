0810be941b832bc68999fc236d8be0a715485689
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-04-07 09:53:25 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      appBar: AppBar(title: Text("Guessing game")),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
+    );
+  }
+}

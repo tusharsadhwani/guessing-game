1ee93ed6ad3c746ee302571642cbf6ae62c0a9ed
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-04-13 07:38:40 -0700

    added join room functionality

--- lib/pages/new_room_page/new_room_page.dart
+++ lib/pages/new_room_page/new_room_page.dart
@@ -130,27 +130,89 @@ class JoinRoom extends StatefulWidget {
 
 class _JoinRoomState extends State<JoinRoom> {
   var joinRoomForm = GlobalKey<FormState>();
+  String userName;
+  int roomCode;
+  String roomId;
+
+  void joinRoom(context) {
+    if (!joinRoomForm.currentState.validate()) return;
+
+    joinRoomForm.currentState.save();
+    final userId = generateUserId();
+    final user = {
+      'name': userName,
+      'id': userId,
+    };
+    Firestore.instance
+        .collection('rooms')
+        .where('id', isEqualTo: roomCode)
+        .getDocuments()
+        .then(
+      (snapshot) async {
+        if (snapshot.documents.length == 0) {
+          showAlert(context, "This room doesn't exist");
+        } else {
+          roomId = snapshot.documents[0].documentID;
+          var roomData = await Firestore.instance
+              .collection('rooms')
+              .document(roomId)
+              .get()
+              .then((snapshot) => snapshot.data);
+
+          (roomData['members'] as List).add(user);
+
+          await Firestore.instance
+              .collection('rooms')
+              .document(roomId)
+              .setData(roomData);
+
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (_) => GamePage(roomId),
+            ),
+          );
+        }
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Form(
+        key: joinRoomForm,
         child: Column(
           children: <Widget>[
             TextFormField(
               decoration: InputDecoration(
                 labelText: "Room Code",
               ),
+              validator: (value) {
+                if (value.length == 0) return "Enter room code";
+                var parsedRoomId = int.tryParse(value);
+                if (parsedRoomId == null) return "Room code must be a number";
+                return null;
+              },
+              onSaved: (value) {
+                roomCode = int.parse(value);
+              },
             ),
             TextFormField(
               decoration: InputDecoration(
                 labelText: "Your Name",
               ),
+              validator: (value) {
+                if (value.length == 0) return "Enter your name";
+                return null;
+              },
+              onSaved: (value) {
+                userName = value;
+              },
             ),
             SizedBox(height: 10),
             RaisedButton(
-              onPressed: () {},
+              onPressed: () => joinRoom(context),
               child: Text('Join Room'),
             )
           ],
@@ -162,3 +224,16 @@ class _JoinRoomState extends State<JoinRoom> {
 
 int generateUserId() => Random().nextInt(100000);
 int generateRoomId() => Random().nextInt(100000000);
+
+void showAlert(BuildContext context, String s) {
+  showDialog(
+    context: context,
+    builder: (_) => AlertDialog(
+      title: Text("Error"),
+      content: Text(
+        s,
+        style: TextStyle(color: Colors.black),
+      ),
+    ),
+  );
+}

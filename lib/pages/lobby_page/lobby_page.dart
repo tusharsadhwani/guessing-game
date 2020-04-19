16e9216b18aff6a768d0e4b0bb789683413e0b55
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-04-19 05:34:27 -0700

    code cleanup

--- lib/pages/lobby_page/lobby_page.dart
+++ lib/pages/lobby_page/lobby_page.dart
@@ -4,8 +4,7 @@ import 'package:flutter/material.dart';
 import '../game_screen/game_screen.dart';
 
 class LobbyPage extends StatefulWidget {
-  static const routeName = '/game';
-  final roomID;
+  final String roomID;
   LobbyPage(this.roomID);
 
   @override
